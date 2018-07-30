06bb1a8edbee250f6bbba55ee4ea376a5d9e46ba
Sara <Sara@appson.ir>
2018-07-30 14:38:40 +0430

    add job control api

---
+++ src/nebula/Controllers/JobActionsController.cs
@@ -0,0 +1,122 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using ComposerCore.Attributes;
+using hydrogen.General.Validation;
+using Nebula.Job;
+
+namespace Nebula.Controllers
+{
+    [Contract]
+    [Component]
+    [ComponentCache(null)]
+    public class JobActionsController : ApiControllerBase
+    {
+        // PUT	/tenant/:tenantId/jobs/:j/actions/resume	-B, IDMP	Changes a job's status back to "InProgress" to continue working normally, if the job is paused or draining.
+        // PUT	/tenant/:tenantId/jobs/:j/actions/pause	-B, IDMP	Changes a job's status to "Paused", and stops workers of the job temporarily until resumed.
+        // PUT	/tenant/:tenantId/jobs/:j/actions/drain	-B, IDMP	Changes a job's status to "Draining", and causes the job queue items to be drained without processing.
+        // PUT	/tenant/:tenantId/jobs/:j/actions/stop	-B, IDMP	Stops a job and all its preprocessors permanently, and deletes all of the job queue items.
+        // PUT	/tenant/:tenantId/jobs/:j/actions/purge-queue	-B, IDMP	Purges the current job queue without any change in the job status.
+
+        [ComponentPlug]
+        public IJobManager JobManager { get; set; }
+
+        [HttpPut]
+        [Route("tenant/{tenantId}/jobs/{jobId}/actions/resume")]
+        public async Task<IHttpActionResult> ResumeJob(string tenantId, string jobId)
+        {
+            var validationResult = ValidateForResumeJob(jobId);
+            if (!validationResult.Success)
+                return ValidationResult(validationResult);
+
+            return ValidationResult(await JobManager.ResumeJob(tenantId, jobId));
+        }
+
+        [HttpPut]
+        [Route("tenant/{tenantId}/jobs/{jobId}/actions/pause")]
+        public async Task<IHttpActionResult> PauseJob(string tenantId, string jobId)
+        {
+            var validationResult = ValidateForPauseJob(jobId);
+            if (!validationResult.Success)
+                return ValidationResult(validationResult);
+
+            return ValidationResult(await JobManager.PauseJob(tenantId, jobId));
+        }
+
+        [HttpPut]
+        [Route("tenant/{tenantId}/jobs/{jobId}/actions/drain")]
+        public async Task<IHttpActionResult> DrainJob(string tenantId, string jobId)
+        {
+            var validationResult = ValidateForDrainJob(jobId);
+            if (!validationResult.Success)
+                return ValidationResult(validationResult);
+
+            return ValidationResult(await JobManager.DrainJob(tenantId, jobId));
+        }
+
+        [HttpPut]
+        [Route("tenant/{tenantId}/jobs/{jobId}/actions/stop")]
+        public async Task<IHttpActionResult> StopJob(string tenantId, string jobId)
+        {
+            var validationResult = ValidateForStopJob(jobId);
+            if (!validationResult.Success)
+                return ValidationResult(validationResult);
+
+            return ValidationResult(await JobManager.StopJob(tenantId, jobId));
+        }
+
+        [HttpPut]
+        [Route("tenant/{tenantId}/jobs/{jobId}/actions/purge-queue")]
+        public async Task<IHttpActionResult> PurgeJobQueue(string tenantId, string jobId)
+        {
+            var validationResult = ValidateForPurgeJobQueue(jobId);
+            if (!validationResult.Success)
+                return ValidationResult(validationResult);
+
+            return ValidationResult(await JobManager.PurgeJobQueue(tenantId, jobId));
+        }
+
+        #region Validation methods
+
+        private ApiValidationResult ValidateForResumeJob(string jobId)
+        {
+            if (string.IsNullOrWhiteSpace(jobId))
+                return ApiValidationResult.Failure(nameof(jobId), ErrorKeys.ArgumentCanNotBeEmpty);
+
+            return ApiValidationResult.Ok();
+        }
+
+        private ApiValidationResult ValidateForPauseJob(string jobId)
+        {
+            if (string.IsNullOrWhiteSpace(jobId))
+                return ApiValidationResult.Failure(nameof(jobId), ErrorKeys.ArgumentCanNotBeEmpty);
+
+            return ApiValidationResult.Ok();
+        }
+
+        private ApiValidationResult ValidateForDrainJob(string jobId)
+        {
+            if (string.IsNullOrWhiteSpace(jobId))
+                return ApiValidationResult.Failure(nameof(jobId), ErrorKeys.ArgumentCanNotBeEmpty);
+
+            return ApiValidationResult.Ok();
+        }
+
+        private ApiValidationResult ValidateForStopJob(string jobId)
+        {
+            if (string.IsNullOrWhiteSpace(jobId))
+                return ApiValidationResult.Failure(nameof(jobId), ErrorKeys.ArgumentCanNotBeEmpty);
+
+            return ApiValidationResult.Ok();
+        }
+
+        private ApiValidationResult ValidateForPurgeJobQueue(string jobId)
+        {
+            if (string.IsNullOrWhiteSpace(jobId))
+                return ApiValidationResult.Failure(nameof(jobId), ErrorKeys.ArgumentCanNotBeEmpty);
+
+            return ApiValidationResult.Ok();
+        }
+
+        #endregion
+    }
+}
---
+++ src/nebula/Controllers/JobInfoController.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ComposerCore.Attributes;
+using hydrogen.General.Collections;
+using Nebula.Controllers.Dto;
+using Nebula.Job;
+using Nebula.Storage;
+using Nebula.Storage.Model;
+
+namespace Nebula.Controllers
+{
+    [Contract]
+    [Component]
+    [ComponentCache(null)]
+    public class JobInfoController : ApiControllerBase
+    {
+        // GET	/tenant/{tenantId}/jobs/list	-B, SAFE	Get the list of running / pending / recently finished jobs
+        // GET	/tenant/{tenantId}/jobs/:j	-B, SAFE Enquiry the status / result of an asynchronous operation, using the ID returned when starting the job
+
+        [ComponentPlug]
+        public IJobStore JobStore { get; set; }
+
+        [ComponentPlug]
+        public IJobManager JobManager { get; set; }
+
+        [HttpGet]
+        [Route("tenant/{tenantId}/jobs/list")]
+        public async Task<IHttpActionResult> GetJobList(string tenantId)
+        {
+            var allJobs = await JobStore.LoadAll(tenantId);
+            return Ok(new GetJobListResponse
+            {
+                Jobs = allJobs.Select(ToGetJobListResponseItem).ToList()
+            });
+        }
+
+        [HttpGet]
+        [Route("tenant/{tenantId}/jobs/{jobId}")]
+        public async Task<IHttpActionResult> GetJobStatus(string tenantId, string jobId)
+        {
+            var jobData = await JobStore.Load(tenantId, jobId);
+            if (jobData == null)
+                return NotFound();
+
+            var result = ToGetJobStatusResponse(jobData);
+
+            // TODO Multiple-loading of job data when calling this API. Once above, and once in getting queue length
+            result.QueueLength = await JobManager.GetQueueLength(tenantId, jobId);
+
+            return Ok(result);
+        }
+
+        #region helpers
+
+        private GetJobListResponseItem ToGetJobListResponseItem(JobData jobData)
+        {
+            return new GetJobListResponseItem
+            {
+                JobId = jobData.JobId,
+                State = jobData.Status.State.ToString(),
+                StateTime = jobData.Status.StateTime,
+                IsCompleted = jobData.Status.State >= JobState.Completed,
+                JobDisplayName = jobData.JobDisplayName,
+                CreationTime = jobData.CreationTime
+            };
+        }
+
+        private GetJobStatusResponse ToGetJobStatusResponse(JobData jobData)
+        {
+            return new GetJobStatusResponse
+            {
+                JobId = jobData.JobId,
+                JobDisplayName = jobData.JobDisplayName,
+                State = jobData.Status.State.ToString(),
+                StateTime = jobData.Status.StateTime,
+                IsCompleted = jobData.Status.State >= JobState.Completed,
+                LastActivityTime = jobData.Status.LastIterationStartTime,
+                LastProcessTime = jobData.Status.LastProcessFinishTime,
+                LastHealthCheckTime = jobData.Status.LastHealthCheckTime,
+                ItemsProcessed = jobData.Status.ItemsProcessed,
+                ItemsFailed = jobData.Status.ItemsFailed,
+                ItemsRequeued = jobData.Status.ItemsRequeued,
+                ItemsGeneratedForTargetQueue = jobData.Status.ItemsGeneratedForTargetQueue,
+                EstimatedTotalItems = jobData.Status.EstimatedTotalItems,
+                ProcessingTimeTakenMillis = jobData.Status.ProcessingTimeTakenMillis,
+                ExceptionCount = jobData.Status.ExceptionCount,
+                LastExceptionTime = jobData.Status.LastExceptionTime,
+                LastFailTime = jobData.Status.LastFailTime,
+                LastFailures = jobData.Status.LastFailures.SafeSelect(f => f.ErrorMessage).ToArray(),
+                CreationTime = jobData.CreationTime,
+                PreprocessorJobIds = jobData.Configuration.PreprocessorJobIds
+            };
+        }
+
+        #endregion
+    }
+}
