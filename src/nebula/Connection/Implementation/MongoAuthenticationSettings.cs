ff26dee6421dc382cc490a36cd7f1ea9617f0360
Sara <Sara@appson.ir>
2018-08-18 11:02:22 +0430

    delete MongoAuthenticationSettings

--- src/nebula/Connection/Implementation/MongoAuthenticationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nebula.Connection.Implementation
-{
-    internal class MongoAuthenticationSettings
-    {
-        public string AuthenticatorDbName { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
