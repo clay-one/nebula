448d411e73c8bc1019a5dfb08c533d3ba1f27b78
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-01-28 18:15:44 +0330

    Add instance provider to register job processor

---
+++ src/nebula/ExternalProviderComponentFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ComposerCore;
+using ComposerCore.Extensibility;
+
+namespace Nebula
+{
+    public class ExternalProviderComponentFactory : IComponentFactory
+    {
+        private readonly Func<object> _instanceProvider;
+
+        public ExternalProviderComponentFactory(Func<object> instanceProvider)
+        {
+            _instanceProvider = instanceProvider;
+        }
+
+        public object GetComponentInstance(ContractIdentity contract, IEnumerable<ICompositionListener> listenerChain)
+        {
+            return _instanceProvider.Invoke();
+        }
+
+        public IEnumerable<Type> GetContractTypes()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IComposer composer)
+        {
+            
+        }
+    }
+}
